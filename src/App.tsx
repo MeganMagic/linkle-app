b941776041459c4811bdaf7f11b9a8161780f6bc
Megan <at.fence.3@gmail.com>
2022-01-19 12:05:00 +0900

    Side Navigation 전역적으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import SideNav from './components/SideNav/SideNav';
 
 const App = () => {
     return (
         <div className="App">
-            <nav className="sidebar">
-
-            </nav>
+            <SideNav />
             
             <div className='page-container'>
                 <Outlet />
