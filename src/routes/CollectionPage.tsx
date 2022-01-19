9cb1f5ec69dada65f29e6e9514ab8638cd2af5f2
Megan <at.fence.3@gmail.com>
2022-01-19 12:06:29 +0900

    collection page 기초 작업

--- src/routes/CollectionPage.tsx
+++ src/routes/CollectionPage.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import LinkSection from '../components/LinkSection';
+import { CollectionType } from '../types';
+import { apiEndPoint, ssoqToken } from '../variables';
+
 
 const CollectionPage = () => {
-    const element =(
-        <div className='Collection-page'>
-            
-        </div>
-    )
+    const param = useParams();
+    const { collectionId } = param;
 
-    return element;
+    const [ isLoading, setIsLoading ] = useState<boolean>(true);
+    const [ data, setData ] = useState<CollectionType>();
+
+    //console.log(collectionId)
+    useEffect(() => {
+        axios.get(apiEndPoint + '/w/collections/' + collectionId, {
+            headers : {
+                'content-type': 'application/json',
+                'SSOQ-TOKEN' : ssoqToken
+            }
+        })
+        .then(res => res.data)
+        .then(data => {
+            setData(data.data);
+            setIsLoading(false)
+        })
+    }, [])
+
+    const introElement = (data : CollectionType) => (
+        <section className='collection-page-intro'>
+            <div className='flex'>
+                <div className='thumbnail' style={{backgroundImage:`url(${data.introImage})`}} ></div>
+                
+                <div className='flex flex-col'>
+                    <div className='title'>{data.name}</div>
+                    <div className='owner'>{data.ownerNames}</div>
+
+                    <div>
+                        <button>참가</button>
+                        <button>구독</button>
+                    </div>
+                </div>
+            </div>
+            
+        </section>
+    );
+
+    return (
+        <div className='Collection-page'>
+            { data && introElement(data) }
+            <LinkSection name='link' fetchUrl={apiEndPoint + '/w/collections/' + collectionId + '/links'} token={ssoqToken} sectionTitle='링크'/>
+        </div>
+    );
 }
 
 export default CollectionPage;
