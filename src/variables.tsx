74b67d17004a161f76bae26ddf62069076abaf16
Megan <at.fence.3@gmail.com>
2022-01-19 12:04:33 +0900

    토큰 재발급

--- src/variables.tsx
+++ src/variables.tsx
@@ -1,2 +1,2 @@
 export const apiEndPoint : string = 'https://api.ssoq.io';
-export const ssoqToken : string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YjBiZWUzZTAxMDcxMWVjODE2M2U3NmEyMDc3NmY4YiIsIm5hbWUiOiLshqHsp4Tqsr0iLCJyb2xlcyI6IlJPTEVfVVNFUiIsImlhdCI6MTY0MTE4Mjk5MywiZXhwIjoxNjQyNDc4OTkzfQ.jT-ROjWN41QrLIxhB92IWs5AmqukvxAjoVzkFXnIaeI';
+export const ssoqToken : string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YjBiZWUzZTAxMDcxMWVjODE2M2U3NmEyMDc3NmY4YiIsIm5hbWUiOiLshqHsp4Tqsr0iLCJyb2xlcyI6IlJPTEVfVVNFUiIsImlhdCI6MTY0MjU1OTU0MCwiZXhwIjoxNjQzODU1NTQwfQ.YtaiF-Yjr0Zz8sjx1MHbfXcmYihKIg9ii05nRhGQPoI';
