e6ada9451428f1221b4ab0d97f4c624a7c2c8f48
Megan <at.fence.3@gmail.com>
2022-02-03 17:11:22 +0900

    token 변경, request option 변수에 추가

--- src/variables.tsx
+++ src/variables.tsx
@@ -1,2 +1,9 @@
 export const apiEndPoint : string = 'https://api.ssoq.io';
 export const ssoqToken : string = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YjBiZWUzZTAxMDcxMWVjODE2M2U3NmEyMDc3NmY4YiIsIm5hbWUiOiLshqHsp4Tqsr0iLCJyb2xlcyI6IlJPTEVfVVNFUiIsImlhdCI6MTY0MjU1OTU0MCwiZXhwIjoxNjQzODU1NTQwfQ.YtaiF-Yjr0Zz8sjx1MHbfXcmYihKIg9ii05nRhGQPoI';
+
+export const requestOption = {
+    headers : {
+        'content-type': 'application/json',
+        'SSOQ-TOKEN' : ssoqToken
+    }
+}
