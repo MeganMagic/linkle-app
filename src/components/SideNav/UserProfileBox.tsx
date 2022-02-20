8a0ff7f27ad90f829d1f6b281f7239bf5e2db04e
Megan <at.fence.3@gmail.com>
2022-02-20 22:59:34 +0900

    user profile box 디자인 수정

--- src/components/SideNav/UserProfileBox.tsx
+++ src/components/SideNav/UserProfileBox.tsx
@@ -1,7 +1,6 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { MyProfileType, UserProfileType } from '../../types';
-import { apiEndPoint } from '../../variables';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { UserProfileType } from '../../types';
 
 type UserProfileBoxProps = {
     data? : UserProfileType
@@ -10,12 +9,30 @@ type UserProfileBoxProps = {
 const UserProfileView : React.FC<UserProfileBoxProps> = ({
     data
 }) => {
-    
+
     return data ? 
-        <div className="user-profile-box">
-            <div className='wrapper flex flex-ai-c'>
+        <div className="user-profile">
+            <div className='profile-box'>
                 <div className='thumbnail' style={{backgroundImage:`url(${data.thumbImage})`}}></div>
-                <div className='name'>{data.name}</div>
+                <div className='name flex flex-ai-c'>
+                    <p className='md'>{data.name}</p>
+                </div>
+
+                <div className='flex flex-ai-c'>
+                    <i className='more bx bx-dots-horizontal-rounded'></i>
+                </div>
+            </div>
+
+            <div className='profile-menu' >
+                <Link to='/profile/my'>
+                    <div className='profile-menu-cell'>
+                        <p className='md-light'>내 프로필</p>
+                    </div>
+                </Link>
+                
+                <div className='profile-menu-cell'>
+                    <p className='md-light'>로그아웃</p>
+                </div>
             </div>
         </div>
         :
